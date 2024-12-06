3b05cdbe208f666898e14323820a87285483b46c
Bradley Bullock <braddyb7@hotmail.com>
2024-12-06 13:03:37 +0000

    Enhance Get-PublicIP function with detailed comment-based help and examples

--- src/public/Get-PublicIP.ps1
+++ src/public/Get-PublicIP.ps1
@@ -1,6 +1,36 @@
-(curl icanhazip.com).Content | clip
-
 function Get-PublicIP {
+    <#
+    .SYNOPSIS
+    Retrieves the public IP address of the machine.
+
+    .DESCRIPTION
+    The Get-PublicIP function uses an external service to fetch the public IP address of the machine.
+    Optionally, it can copy the IP address to the clipboard.
+
+    .SYNTAX
+    Get-PublicIP [[-CopyToClipboard] [<SwitchParameter>]]
+
+    .PARAMETER CopyToClipboard
+    If specified, the public IP address will be copied to the clipboard.
+
+    .OUTPUTS
+    System.String
+    The public IP address.
+
+    .EXAMPLE
+    PS C:\> Get-PublicIP
+    203.0.113.42
+
+    .EXAMPLE
+    PS C:\> Get-PublicIP -CopyToClipboard
+    203.0.113.42
+    The IP address is copied to the clipboard.
+
+    .NOTES
+    Author: Bradley Bullock
+
+    #>
+
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $false)]
