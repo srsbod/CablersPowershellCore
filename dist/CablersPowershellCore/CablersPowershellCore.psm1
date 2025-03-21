dd3fef84fa64a10c1850d255a891e577aeba7c3a
Bradley Bullock <yourusername@users.noreply.github.com>
2025-03-21 09:31:11 +0000

    Update module manifest

--- dist/CablersPowershellCore/CablersPowershellCore.psm1
+++ dist/CablersPowershellCore/CablersPowershellCore.psm1
@@ -1,9 +1,17 @@
 # Import private functions
-Get-ChildItem -Path "$PSScriptRoot\..\..\src\private" -Filter *.ps1 | ForEach-Object {
-    . $_.FullName
+foreach ($file in Get-ChildItem -Path "$PSScriptRoot\..\..\src\private" -Filter *.ps1) {
+    . $file.FullName
 }
 
 # Import public functions
-Get-ChildItem -Path "$PSScriptRoot\..\..\src\public" -Filter *.ps1 | ForEach-Object {
-    . $_.FullName
+foreach ($file in Get-ChildItem -Path "$PSScriptRoot\..\..\src\public" -Filter *.ps1) {
+    . $file.FullName
 }
+
+Set-Alias -name Get-InstalledApps -Value Get-InstalledSoftware
+Set-Alias -name Uninstall-Apps -Value Uninstall-Software
+Set-Alias -name ip -Value Get-InternalIP
+Set-Alias -name iplocation -Value Get-IPAddressLocation
+Set-Alias -name uptime -Value Get-Uptime
+Set-Alias -name lastboot -Value Get-LastBootTime
+Set-Alias -name publicip -Value Get-PublicIP
