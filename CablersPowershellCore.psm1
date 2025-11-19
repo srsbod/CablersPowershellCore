8c79cbafa64e3cc7bb2de6659ff49e544568767e
Bradley Bullock <439144+srsbod@users.noreply.github.com>
2025-11-19 14:31:31 +0000

    style: correct casing for alias names feat: add isAdmin alias for Test-IsAdmin

--- CablersPowershellCore.psm1
+++ CablersPowershellCore.psm1
@@ -8,13 +8,14 @@ foreach ($file in Get-ChildItem -Path "$PSScriptRoot\src\public" -Filter *.ps1) 
     . $file.FullName
 }
 
-Set-Alias -name Get-InstalledApps -Value Get-InstalledSoftware
-Set-Alias -name Uninstall-Apps -Value Uninstall-Software
-Set-Alias -name ip -Value Get-InternalIP
-Set-Alias -name iplocation -Value Get-IPAddressLocation
-Set-Alias -name uptime -Value Get-Uptime
-Set-Alias -name lastboot -Value Get-LastBootTime
-Set-Alias -name publicip -Value Get-PublicIP
-Set-Alias -name isempty -Value Test-EmptyFolder
+Set-Alias -Name Get-InstalledApps -Value Get-InstalledSoftware
+Set-Alias -Name Uninstall-Apps -Value Uninstall-Software
+Set-Alias -Name ip -Value Get-InternalIP
+Set-Alias -Name ipLocation -Value Get-IPAddressLocation
+Set-Alias -Name uptime -Value Get-Uptime
+Set-Alias -Name lastBoot -Value Get-LastBootTime
+Set-Alias -Name publicIP -Value Get-PublicIP
+Set-Alias -Name isEmpty -Value Test-EmptyFolder
+Set-Alias -Name isAdmin -Value Test-IsAdmin
 
 
